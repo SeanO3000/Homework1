fdff79249e67e2a57a1727d8584c98ef90a96764
Sean O'Dell <38016179+SeanO3000@users.noreply.github.com>
2022-04-24 06:51:02 -0700

    New Project BirthdayCardGenerator

---
+++ BirthdayCardGenerator/Models/BirthdayMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace BirthdayCardGenerator.Models
+{
+    public class BirthdayMessage
+    {
+
+        public string? sender { get; set; }
+        public string? recipient { get; set; }
+        public string? message { get; set; }
+
+    }
+}
